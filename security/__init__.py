e3279972ce12483879c736060f09b2911db8f8aa
s3nh <17111860+s3nh@users.noreply.github.com>
2025-12-15 13:46:10 +0100

    Add security module initialization

--- security/__init__.py
+++ security/__init__.py
@@ -1,1 +1,17 @@
+"""Security module for document processing guardrails"""
 
+from .guardrails import GuardrailsEngine, GuardrailResult
+from .injection_detector import PromptInjectionDetector, InjectionCheckResult
+from .input_sanitizer import InputSanitizer, SanitizationResult
+from .output_filter import OutputFilter, FilterResult
+
+__all__ = [
+    "GuardrailsEngine",
+    "GuardrailResult",
+    "PromptInjectionDetector",
+    "InjectionCheckResult",
+    "InputSanitizer",
+    "SanitizationResult",
+    "OutputFilter",
+    "FilterResult",
+]
